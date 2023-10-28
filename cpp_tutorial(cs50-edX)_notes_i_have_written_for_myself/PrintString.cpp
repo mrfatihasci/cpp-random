86d874df66d3f06851648f865f3d0dbace1f84f9
mrfatihasci <126919157+mrfatihasci@users.noreply.github.com>
2023-10-28 15:34:41 +0300

    add new C++ folder with a bunch of C++ tutorial notes inside of it to the repository
    This basic codes are about PrintString, learning memory in C++, and learning how to swap and not swap variables in C++.

---
+++ cpp_tutorial(cs50-edX)_notes_i_have_written_for_myself/PrintString.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+
+
+using namespace std;
+int main(void)
+{
+    string s get_string("HI!");
+
+    std::cout << s << "\n";
+}
+
---
+++ cpp_tutorial(cs50-edX)_notes_i_have_written_for_myself/memory.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n = 50;
+    printf("%p\n", &n);         //wherever the int n is, print it's location where it is stored in the memory 
+}
+
